658d1578802241188af762952fddc71d30038c60
Farhan Ahmad Primaditya <farhan.primaditya@gmail.com>
2022-03-06 16:41:17 +0700

    Testing deployment 9

--- src/main/java/com/propensi/a03/repository/TutorialRepository.java
+++ src/main/java/com/propensi/a03/repository/TutorialRepository.java
@@ -1,10 +1,10 @@
-package com.propensi.a03.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.propensi.a03.model.Tutorial;
-
-public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-    List<Tutorial> findByPublished(boolean published);
-    List<Tutorial> findByTitleContaining(String title);
-}
+//package com.propensi.a03.repository;
+//
+//import java.util.List;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import com.propensi.a03.model.Tutorial;
+//
+//public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+//    List<Tutorial> findByPublished(boolean published);
+//    List<Tutorial> findByTitleContaining(String title);
+//}
