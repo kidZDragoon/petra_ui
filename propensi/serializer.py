611e5ba9b31bbb92ec4e65af01fda5713e1c2a07
Farhan Ahmad Primaditya <farhan.primaditya@gmail.com>
2022-03-21 20:17:40 +0700

    Initial commit for react-django stack

---
+++ propensi/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User, Profile
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'org_code', 'role', 'npm', 'faculty', 'study_program', 'educational_program')
