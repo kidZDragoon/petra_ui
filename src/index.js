9e844d2164020ed343f734489f86bf52ac9c5b3b
rizmachaerani <rizmachaerani@gmail.com>
2022-03-22 13:35:37 +0700

    setup pbi7

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
     <HashRouter>
-        <App />
+    <App />
+    <Routes>
+        <Route path="/KaryaIlmiah/:id" element={<Detail/>}/>
+        <Route path="/VerifikasiKaryaIlmiah" element={<Detail/>}></Route>
+    </Routes>
     </HashRouter>,
 
     document.getElementById("root")
+
 );
+
 
 
