66e9e030cb75fe6e917c01557896719db498a5cc
Farah Mumtaza Budiawan <farah.mumtaza@ui.ac.id>
2022-04-15 10:55:42 +0700

    PBI 6 - Pengunggahan django

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Detail from "./components/detail";
 import KaryaIlmiahSaya from './components/karya-ilmiah-saya';
 import DaftarVerifikasi from './components/verifikasi';
 import UploadKaryaIlmiah from "./components/unggah-karya-ilmiah";
-import SeachList from "./components/cariKaril";
+import SearchList from "./components/cariKaril";
 import AdvancedSearch from './components/advancedSearch';
 
 
@@ -17,7 +17,7 @@ ReactDOM.render(
             <Route path="/KaryaIlmiah/:id" element={<Detail/>}/>
             <Route path='/DaftarVerifikasi' element={<DaftarVerifikasi/>}/>
             <Route path="/karya-ilmiah-saya/upload" element={<UploadKaryaIlmiah/>} />
-            <Route path="/Search" element={<SeachList/>}/>
+            <Route path="/Search" element={<SearchList/>}/>
             <Route path="/AdvancedSearch" element={<AdvancedSearch/>}/>
             <Route path="/karya-ilmiah-saya" element={<KaryaIlmiahSaya/>} />
         </Routes>
