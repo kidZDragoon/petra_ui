3049c80778c8eb15b059c9120a3024448b487269
Farhan Ahmad Primaditya <farhan.primaditya@gmail.com>
2022-05-16 14:52:59 +0700

    refactor: set api to production & remove old springboot files

--- src/main/frontend/src/http-common.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-export default axios.create({
-    baseURL: "https://propensi-a03.herokuapp.com/api",
-    headers: {
-        "Content-type": "application/json"
-    }
-});
