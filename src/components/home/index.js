b12a90f1c11343890168d8fb40354fe3faae7da2
Fikra Shafna <shafna.fikra@gmail.com>
2022-05-17 21:38:19 +0700

    feat: responsiveness landing page for desktop

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -11,7 +11,9 @@ import {
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import { Search } from "@mui/icons-material";
-
+import Home1 from '../../Home1.svg'
+import Home2 from '../../Home2.svg'
+import Grid from '@mui/material/Grid';
 
 export default class Home extends Component {
     constructor(props) {
@@ -45,65 +47,80 @@ export default class Home extends Component {
 
     render(){
         return(
-            <Container >
-                <div className={classes.section}>
-                    <div  className={classes.home}>
-                    <img
-                    src={logo_text}
-                    width="250"
-                    height="120"
-                    className="d-inline-block align-top"
-                    alt="React Bootstrap logo"
-                    id={classes["logo_text"]}/>
-                    {/* <p>Perpustakaan Digital Departemen<br/>Kesejahteraan Sosial FISIP  UI</p> */}
-                    <h2  className={classes.jargon}>Jelajahi karya ilmiah tentang kesejahteraan sosial. </h2>
-                    
-                    <Box my={5}>
-                    <TextField
-                        className={classes.search_bar}
-                        label="Cari Karya Ilmiah Berdasarkan Judul, Penulis, atau Kata Kunci"
-                        fullWidth
-                        value={this.state.keyword}
-                        onChange={(event) => this.setState({keyword: event.target.value})}
-                        onKeyPress={(ev) => {
-                            if (ev.key === "Enter") {
-                            this.pencarian()
-                            }
-                        }}
-                        InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="start">
-                            <Search />
-                            </InputAdornment>
-                        ),
-                        // style: { borderRadius: 8 }
-                        }}
-                    />
-                    </Box> 
+            <>
+                <Grid container spacing={2}>
+                    <Grid item xs={3}>
+                        <img src={Home1} alt="gambar1" />
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Container>
+                            <div className={classes.section}>
+                                <div className={classes.home}>
+                                    <img
+                                        src={logo_text}
+                                        width="250"
+                                        height="120"
+                                        className="d-inline-block align-top"
+                                        alt="React Bootstrap logo"
+                                        id={classes["logo_text"]} />
+                                    <h2 className={classes.jargon}>Jelajahi karya ilmiah tentang<br/>kesejahteraan sosial.</h2>
 
-                    </div>
-                                   
-                </div>
-               
-                <div className={classes.pengumuman}>
-                    <div class="d-flex justify-content-between">
-                        <div class="p-2"><h3 className={classes.judulPengumuman}>Pengumuman</h3></div>
-                        <div class="p-2"><Link to="/list-pengumuman" className={classes.link}>
-                            lihat semua</Link></div>
-                    
-                    </div>
-                {this.state.listPengumuman.map((pengumuman) => (
-                    <CardPengumuman judul={pengumuman.judul} tglDibuat={pengumuman.tglDibuat} 
-                    pesan={pengumuman.isiPengumuman} id={pengumuman.id} isStaf={false}/>
-                ))}
-              
-                </div>
+                                    <Box my={5}>
+                                        <TextField
+                                            className={classes.search_bar}
+                                            label="Cari Karya Ilmiah Berdasarkan Judul, Penulis, atau Kata Kunci"
+                                            fullWidth
+                                            value={this.state.keyword}
+                                            onChange={(event) => this.setState({ keyword: event.target.value })}
+                                            onKeyPress={(ev) => {
+                                                if (ev.key === "Enter") {
+                                                    this.pencarian();
+                                                }
+                                            } }
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <InputAdornment position="start">
+                                                        <Search />
+                                                    </InputAdornment>
+                                                ),
+                                            }} />
+                                    </Box>
+
+                                </div>
+
+                            </div>
+                        </Container>
+                    </Grid>
+                </Grid>
+
+                <Grid container spacing={2}>
+                    <Grid item xs={9}>
+                        <Container>
+                            <div className={classes.section_pengumuman}>
+                                <div className={classes.pengumuman}>
+                                    <div class="d-flex justify-content-between">
+                                        <div class="p-2"><h3 className={classes.judulPengumuman}>Berita Terbaru</h3></div>
+                                        <div class="p-2"><Link to="/list-pengumuman" className={classes.link}>
+                                            lihat semua berita</Link></div>
+
+                                    </div>
+                                    {this.state.listPengumuman.map((pengumuman) => (
+                                        <CardPengumuman judul={pengumuman.judul} tglDibuat={pengumuman.tglDibuat}
+                                            pesan={pengumuman.isiPengumuman} id={pengumuman.id} isStaf={false} />
+                                    ))}
+
+                                </div>
+                            </div>
+                        </Container>
+                    </Grid>
+                    <Grid item xs={3} display="flex" justifyContent="flex-end">
+                        {/* <div className={classes.space}> */}
+                            <img src={Home2} alt="gambar2" />
+                        {/* </div> */}
+                    </Grid>
+                </Grid>
                 
-           
-                    <div className={classes.space}>
-
-                    </div>
-            </Container>
+            </>
         )
     }
 }
